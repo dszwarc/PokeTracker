90c538204208496208459dac5f1cfd9b05086626
dszwarc <dszwarc93@gmail.com>
2023-05-12 09:56:28 -0400

    add atlas mongodb

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/PokeTracker', {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true
 });
+
 
 const db = mongoose.connection;
 
